13ebc6a517f6fc269fd24af7036f0e92ce9bc818
teenager <teenagerdine@gmail.com>
2024-11-01 00:22:00 +0100

    Fix encoding issues for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ajoutez ceci
   build: {
     outDir: 'dist',
+    // Ajout de la configuration pour gérer l'encodage
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
   server: {
     port: 3000
   },
-  // Ajout de la configuration pour gérer les assets publics
   publicDir: 'public'
 })
