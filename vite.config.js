94bb2391e28a84c2539795f4100a947d04346833
teenager <teenagerdine@gmail.com>
2024-11-01 00:15:01 +0100

    Fix favicon and build configuration

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Ajoutez ceci
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000
+  },
+  // Ajout de la configuration pour gérer les assets publics
+  publicDir: 'public'
 })
